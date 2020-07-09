c06e29b07022810d7ad0c337bdaf620d314c2295
João Apostulo <japostulo@gmail.com>
2020-07-09 15:18:54 -0300

    Primeira parte do projeto monster

---
+++ app.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:'#app',
+    data:{
+        running: false,
+        playerLife:100,
+        monsterLife:100,
+    },
+    computed:{
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
+    },
+    methods:{
+        startGame(){
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
+    },
+    watch:{
+
+    }
+});
